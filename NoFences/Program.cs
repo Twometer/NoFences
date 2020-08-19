f53ded210706741ec3d643f8583d269079516540
Twometer <twometer@outlook.de>
2020-08-20 00:55:04 +0200

    Remove unnecessary code

--- NoFences/Program.cs
+++ NoFences/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -8,10 +6,6 @@ namespace NoFences
 {
     static class Program
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
-
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
