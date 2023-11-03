d98fd3f7f7b94d51e896c320bf20c307c59b1914
QIVD <you@example.com>
2023-11-03 21:20:23 +0800

    Enable control text internationalization Add zh-CN localization

--- NoFences/EditDialog.Designer.cs
+++ NoFences/EditDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditDialog));
             this.lbName = new System.Windows.Forms.Label();
             this.tbName = new System.Windows.Forms.TextBox();
             this.btnOk = new System.Windows.Forms.Button();
@@ -36,50 +37,37 @@
             // 
             // lbName
             // 
-            this.lbName.AutoSize = true;
-            this.lbName.Location = new System.Drawing.Point(26, 27);
+            resources.ApplyResources(this.lbName, "lbName");
             this.lbName.Name = "lbName";
-            this.lbName.Size = new System.Drawing.Size(38, 13);
-            this.lbName.TabIndex = 0;
-            this.lbName.Text = "Name:";
             // 
             // tbName
             // 
-            this.tbName.Location = new System.Drawing.Point(70, 24);
+            resources.ApplyResources(this.tbName, "tbName");
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(281, 20);
-            this.tbName.TabIndex = 1;
             // 
             // btnOk
             // 
+            resources.ApplyResources(this.btnOk, "btnOk");
             this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnOk.Location = new System.Drawing.Point(258, 64);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(56, 23);
-            this.btnOk.TabIndex = 2;
-            this.btnOk.Text = "OK";
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // btnCancel
             // 
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(320, 64);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(56, 23);
-            this.btnCancel.TabIndex = 3;
-            this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // EditDialog
             // 
             this.AcceptButton = this.btnOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(388, 104);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
             this.Controls.Add(this.tbName);
@@ -88,8 +76,6 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "EditDialog";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Rename";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- NoFences/FenceWindow.Designer.cs
+++ NoFences/FenceWindow.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FenceWindow));
             this.appContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.deleteItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lockedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -43,6 +44,7 @@
             // 
             // appContextMenu
             // 
+            resources.ApplyResources(this.appContextMenu, "appContextMenu");
             this.appContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.deleteItemToolStripMenuItem,
             this.lockedToolStripMenuItem,
@@ -54,72 +56,62 @@
             this.exitToolStripMenuItem});
             this.appContextMenu.Name = "contextMenuStrip1";
             this.appContextMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.appContextMenu.Size = new System.Drawing.Size(152, 164);
             this.appContextMenu.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // deleteItemToolStripMenuItem
             // 
-            this.deleteItemToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.deleteItemToolStripMenuItem, "deleteItemToolStripMenuItem");
             this.deleteItemToolStripMenuItem.Name = "deleteItemToolStripMenuItem";
-            this.deleteItemToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.deleteItemToolStripMenuItem.Text = "Delete item";
             this.deleteItemToolStripMenuItem.Click += new System.EventHandler(this.deleteItemToolStripMenuItem_Click);
             // 
             // lockedToolStripMenuItem
             // 
+            resources.ApplyResources(this.lockedToolStripMenuItem, "lockedToolStripMenuItem");
             this.lockedToolStripMenuItem.CheckOnClick = true;
             this.lockedToolStripMenuItem.Name = "lockedToolStripMenuItem";
-            this.lockedToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.lockedToolStripMenuItem.Text = "Lock";
             this.lockedToolStripMenuItem.Click += new System.EventHandler(this.lockedToolStripMenuItem_Click);
             // 
             // minifyToolStripMenuItem
             // 
+            resources.ApplyResources(this.minifyToolStripMenuItem, "minifyToolStripMenuItem");
             this.minifyToolStripMenuItem.CheckOnClick = true;
             this.minifyToolStripMenuItem.Name = "minifyToolStripMenuItem";
-            this.minifyToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.minifyToolStripMenuItem.Text = "Minify";
             this.minifyToolStripMenuItem.Click += new System.EventHandler(this.minifyToolStripMenuItem_Click);
             // 
             // renameToolStripMenuItem
             // 
+            resources.ApplyResources(this.renameToolStripMenuItem, "renameToolStripMenuItem");
             this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
-            this.renameToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.renameToolStripMenuItem.Text = "Rename";
             this.renameToolStripMenuItem.Click += new System.EventHandler(this.renameToolStripMenuItem_Click);
             // 
             // titleSizeToolStripMenuItem
             // 
+            resources.ApplyResources(this.titleSizeToolStripMenuItem, "titleSizeToolStripMenuItem");
             this.titleSizeToolStripMenuItem.Name = "titleSizeToolStripMenuItem";
-            this.titleSizeToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.titleSizeToolStripMenuItem.Text = "Title size...";
             this.titleSizeToolStripMenuItem.Click += new System.EventHandler(this.titleSizeToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(148, 6);
             // 
             // newFenceToolStripMenuItem
             // 
+            resources.ApplyResources(this.newFenceToolStripMenuItem, "newFenceToolStripMenuItem");
             this.newFenceToolStripMenuItem.Name = "newFenceToolStripMenuItem";
-            this.newFenceToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.newFenceToolStripMenuItem.Text = "New Fence";
             this.newFenceToolStripMenuItem.Click += new System.EventHandler(this.newFenceToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
-            this.exitToolStripMenuItem.Text = "Remove Fence";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // FenceWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(338, 110);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -127,8 +119,6 @@
             this.Name = "FenceWindow";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "New Fence";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FenceWindow_FormClosed);
             this.Load += new System.EventHandler(this.FenceWindow_Load);
             this.LocationChanged += new System.EventHandler(this.FenceWindow_LocationChanged);
